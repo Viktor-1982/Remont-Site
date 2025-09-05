68fc7e0e0c7ba134ed1fd7ba3304a46c233d2e3f
Viktor Personal <vles8878@gmail.com>
2025-09-05 10:43:10 +0300

    MDX Renderer fix

--- src/components/mdx-renderer.tsx
+++ src/components/mdx-renderer.tsx
@@ -1,9 +1,13 @@
 ﻿"use client"
 
 import { useMDXComponent } from "next-contentlayer2/hooks"
-import { MDXComponents } from "@/components/mdx-components"
+import type { MDXComponents } from "mdx/types"
+import { MDXComponents as CustomMDXComponents } from "@/components/mdx-components"
 
 export function MDXRenderer({ code }: { code: string }) {
     const MDXContent = useMDXComponent(code)
-    return <MDXContent components={MDXComponents as any} />
+
+    return (
+        <MDXContent components={CustomMDXComponents as MDXComponents} />
+    )
 }
