954bb8fb8390ab7d7a11d82a2eb7ef840cfec4ce
Viktor Personal <vles8878@gmail.com>
2025-09-29 21:20:35 +0300

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T566PSZF');
+            })(window,document,'script','dataLayer','GTM-N2Z2CSMS');
           `}
             </Script>
         </head>
@@ -62,7 +62,7 @@ export default function RootLayout({
         {/* GTM noscript для браузеров без JS */}
         <noscript>
             <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-T566PSZF"
+                src="https://www.googletagmanager.com/ns.html?id=GTM-N2Z2CSMS"
                 height="0"
                 width="0"
                 style={{ display: "none", visibility: "hidden" }}
