6854b434eddfb87d881a205dfb8c473a3d796311
Viktor Personal <vles8878@gmail.com>
2025-10-08 12:53:15 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,12 +51,18 @@ export default async function RootLayout({
             className={`${GeistSans.variable} ${GeistMono.variable}`}
         >
         <head>
+            {/* ✅ Pinterest Domain Verification */}
+            <meta
+                name="p:domain_verify"
+                content="c5936504ab784c7854df0c0807478575"
+            />
+
             <Script id="gtm-script" strategy="afterInteractive">
                 {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N2Z2CSMS');`}
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N2Z2CSMS');`}
             </Script>
         </head>
 
