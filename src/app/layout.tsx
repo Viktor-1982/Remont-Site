aa241f2bf9098d0c9194f5262b9726ca1a4c63cc
Viktor Personal <vles8878@gmail.com>
2025-11-24 23:37:48 +0300

    fix: update Yandex.Metrika code to official version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,21 +93,11 @@ export default async function RootLayout({
                 {`(function(m,e,t,r,i,k,a){
             m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();
-            for (var j=0;j<document.scripts.length;j++){
-              if(document.scripts[j].src===r){return;}
-            }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104537151', 'ym');
 
-          ym(104537151,'init',{
-            ssr:true,
-            webvisor:true,
-            clickmap:true,
-            ecommerce:"dataLayer",
-            accurateTrackBounce:true,
-            trackLinks:true
-          });`}
+          ym(104537151, 'init', {ssr:true, webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});`}
             </Script>
         </head>
 
